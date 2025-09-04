0154c580ba2454224a6e6f48a6a53c2c03d396d4
shreyashrestha <sshr0033@student.monash.edu>
2025-09-05 08:26:44 +1000

    fixed  frontend submodule, added .gitignore for db, updated CORS, include PDF

--- digitalCitizen/src/main/java/com/dicurecitizen/digitalCitizen/config/CorsConfig.java
+++ digitalCitizen/src/main/java/com/dicurecitizen/digitalCitizen/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.dicurecitizen.digitalCitizen.Config;
+package com.dicurecitizen.digitalCitizen.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,9 +17,16 @@ public class CorsConfig {
         cfg.setAllowedMethods(List.of("GET","POST","OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setAllowCredentials(true);
+        cfg.setAllowedOriginPatterns(List.of(
+                "http://localhost:5173",
+                "https://sshr0033.github.io"
+        ));
+        cfg.setExposedHeaders(List.of("Content-Disposition"));
+
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", cfg);
         return new CorsFilter(source);
+
     }
 }

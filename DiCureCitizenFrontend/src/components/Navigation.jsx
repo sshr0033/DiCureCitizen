4caa539041a2ec74623ef1656c734954cf7f04d1
Sijie Fan <sfan0043@student.monash.edu>
2025-09-06 00:12:18 +1000

    Change logo

--- DiCureCitizenFrontend/src/components/Navigation.jsx
+++ DiCureCitizenFrontend/src/components/Navigation.jsx
@@ -1,13 +1,12 @@
 import { Link, NavLink } from 'react-router-dom'
-import logo from '../assets/logo.jpg'
+import logo from '../assets/logo.png'
 
 export default function Navigation() {
   return (
     <header className="header">
       <div className="container header-inner">
         <Link to="/" className="brand">
-        <img src={logo} alt="Logo" className="logo-icon"/>
-        DCC</Link>
+        <img src={logo} alt="Logo" className="logo-icon"/></Link>
         <nav className="nav">
           <NavLink to="/">Detect Spam</NavLink>
           <NavLink to="/trends">Discover More</NavLink>

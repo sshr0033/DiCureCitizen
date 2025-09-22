dbad3541847627dead406336c42072e47c81d55a
Dexter-08 <sshr0033@student.monash.edu>
2025-09-22 11:32:51 +1000

    Update main.py

--- ml/main.py
+++ ml/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import AutoTokenizer, AutoModel
 import uvicorn
+from download_weights import local_path 
 
 OUT_DIR = "./artifacts"
 MODEL_NAME = "roberta-base"

f37d00c00393ddc030746e26232bc208754d91a2
shreyashrestha <sshr0033@student.monash.edu>
2025-09-23 09:31:11 +1000

    changes to the deployment

--- DiCureCitizenFrontend/src/api.ts
+++ DiCureCitizenFrontend/src/api.ts
@@ -3,8 +3,6 @@ const API_URL = import.meta.env.VITE_API_URL;
 export const getConversations = async () => {
   console.log("API URL:", API_URL);  
   const response = await fetch(`${API_URL}/api/conversations`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch conversations");
-  }
+  if (!response.ok) throw new Error("Failed to fetch conversations");
   return response.json();
 };

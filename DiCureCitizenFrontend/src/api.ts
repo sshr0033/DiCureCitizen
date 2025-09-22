98c05a3b34acf9602fba29a0fa4842c8d53ef4d9
shreyashrestha <sshr0033@student.monash.edu>
2025-09-23 00:46:18 +1000

    deployment issue solution

--- DiCureCitizenFrontend/src/api.ts
+++ DiCureCitizenFrontend/src/api.ts
@@ -1,10 +1,7 @@
-
-
 export const getConversations = async () => {
-  const response = await fetch("/api/conversations"); 
+  const response = await fetch("http://169.224.230.28:8080/api/conversations"); 
   if (!response.ok) {
     throw new Error("Failed to fetch conversations");
   }
   return response.json();
 };
-

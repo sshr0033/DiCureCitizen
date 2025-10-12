7b273c63a56dfd64ec8d1234bf049ad45ef80bcc
shreyashrestha <sshr0033@student.monash.edu>
2025-10-12 19:40:36 +1100

    Changing variable name

--- DiCureCitizenFrontend/src/api.ts
+++ DiCureCitizenFrontend/src/api.ts
@@ -1,16 +1,16 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 
 
 export const getConversations = async () => {
-  console.log("API URL:", API_URL);  
-  const response = await fetch(`${API_URL}/api/conversations`);
+  console.log("API URL:", BASE_URL);  
+  const response = await fetch(`${BASE_URL}/api/conversations`);
   if (!response.ok) throw new Error("Failed to fetch conversations");
   return response.json();
 };
 
 export async function predictText(text: string) {
-  const res = await fetch(`${API_URL}/api/predict`, {
+  const res = await fetch(`${BASE_URL}/api/predict`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ text }),

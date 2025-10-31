f4ba9d0b6748323995ed93531041db080f69de91
shreyashrestha <sshr0033@student.monash.edu>
2025-10-31 15:44:46 +1100

    Removing the Password requirement

--- DiCureCitizenFrontend/src/App.tsx
+++ DiCureCitizenFrontend/src/App.tsx
@@ -3,7 +3,6 @@ import { theme } from "./theme";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DetectScam from "./pages/DetectSpam";
 import HomePage from "./pages/HomePage";
-import LoginPage from "./components/LoginPage";
 import QuizAndLessons from "./pages/QuizAndLessons";
 import NearestHelpPage from "./pages/NearestHelpPage";
 import ScamAudio from "./pages/ScamAudioPage";
@@ -15,7 +14,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
-        <LoginPage>
+        
           <Routes>
             <Route path="/detectscam" element={<DetectScam />} />
             <Route path="/" element={<HomePage />} />
@@ -25,7 +24,7 @@ export default function App() {
             <Route path="/helpcenter" element={<NearestHelpPage />} />
             <Route path="/resources" element={<ResourcePage />} />
           </Routes>
-        </LoginPage>
+        
       </Router>
     </ThemeProvider>
   );
